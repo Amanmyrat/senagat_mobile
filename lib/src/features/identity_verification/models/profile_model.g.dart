9600ad10cbdc36f44966eb0dead6a157c1f31aaf
Begmyrat <tekemyradowbegmyrat8@gmail.com>
2025-10-07 19:26:41 +0500

    api for profile done

---
+++ lib/src/features/identity_verification/models/profile_model.g.dart
@@ -0,0 +1,65 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'profile_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ProfileModelAdapter extends TypeAdapter<ProfileModel> {
+  @override
+  final int typeId = 2;
+
+  @override
+  ProfileModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return ProfileModel(
+      firstName: fields[0] as String?,
+      lastName: fields[1] as String?,
+      middleName: fields[2] as String?,
+      birthDate: fields[3] as String?,
+      passportNumber: fields[4] as String?,
+      gender: fields[5] as String?,
+      issuedDate: fields[6] as String?,
+      issuedBy: fields[7] as String?,
+      getPassportScan: fields[8] as String?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, ProfileModel obj) {
+    writer
+      ..writeByte(9)
+      ..writeByte(0)
+      ..write(obj.firstName)
+      ..writeByte(1)
+      ..write(obj.lastName)
+      ..writeByte(2)
+      ..write(obj.middleName)
+      ..writeByte(3)
+      ..write(obj.birthDate)
+      ..writeByte(4)
+      ..write(obj.passportNumber)
+      ..writeByte(5)
+      ..write(obj.gender)
+      ..writeByte(6)
+      ..write(obj.issuedDate)
+      ..writeByte(7)
+      ..write(obj.issuedBy)
+      ..writeByte(8)
+      ..write(obj.getPassportScan);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ProfileModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
