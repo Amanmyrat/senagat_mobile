3c3395edf20a98b5055e1482c202b0e371d28e89
Begmyrat <tekemyradowbegmyrat8@gmail.com>
2026-03-24 16:19:09 +0500

    added animation for header widget

--- lib/src/widgets/header_widget.dart
+++ lib/src/widgets/header_widget.dart
@@ -34,54 +34,55 @@ class _HeaderWidgetState extends State<HeaderWidget> {
       ),
       builder: (controller) {
         return Padding(
-          padding: EdgeInsets.only(bottom: 22.h),
+          padding: EdgeInsets.only(bottom: 0.h),
           child: Row(
             children: [
-              Expanded(
-                child: TextFormField(
-                  onTap: () {
-                    Get.toNamed(WebViewScreen.route);
-                    // Get.toNamed(MapSearchScreen.route);
-                  },
-                  readOnly: true,
-                  keyboardType: TextInputType.text,
-                  maxLength: 8,
-                  style: TextStyle(fontSize: 14.sp, fontFamily: AppFonts.primaryFont),
-                  decoration: InputDecoration(
-                    hintText: r'find_an_ATM'.tr,
-                    border: OutlineInputBorder(),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(
-                        AppDimensions.borderRadiusMedium,
-                      ),
-                      borderSide: BorderSide(color: AppColors.white, width: 1.w),
-                    ),
-                    suffixIconConstraints: BoxConstraints(
-                      minWidth: 20.w,
-                      minHeight: 20.h,
-                    ),
-                    suffixIcon: Padding(
-                      padding: EdgeInsets.only(
-                        left: AppDimensions.paddingMedium.w,
-                        right: AppDimensions.paddingExtraLarge.w,
-                      ),
-                      child: SvgPicture.asset(AppAssets.buildingApartmentIcon, width: 20.w),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(
-                        AppDimensions.borderRadiusMedium,
-                      ),
-                      borderSide: BorderSide(color: AppColors.white, width: 1.w),
-                    ),
-
-                    counter: const SizedBox(),
-                    contentPadding: EdgeInsets.symmetric(
-                      vertical: 16.h,
-                      horizontal: AppDimensions.paddingLarge.w,
-                    ),
-                  ),
-                ),
-              ),
+              // Expanded(
+              //   child: TextFormField(
+              //     onTap: () {
+              //       Get.toNamed(WebViewScreen.route);
+              //       // Get.toNamed(MapSearchScreen.route);
+              //     },
+              //     readOnly: true,
+              //     keyboardType: TextInputType.text,
+              //     maxLength: 8,
+              //     style: TextStyle(fontSize: 14.sp, fontFamily: AppFonts.primaryFont),
+              //     decoration: InputDecoration(
+              //       hintText: r'find_an_ATM'.tr,
+              //       border: OutlineInputBorder(),
+              //       focusedBorder: OutlineInputBorder(
+              //         borderRadius: BorderRadius.circular(
+              //           AppDimensions.borderRadiusMedium,
+              //         ),
+              //         borderSide: BorderSide(color: AppColors.white, width: 1.w),
+              //       ),
+              //       suffixIconConstraints: BoxConstraints(
+              //         minWidth: 20.w,
+              //         minHeight: 20.h,
+              //       ),
+              //       suffixIcon: Padding(
+              //         padding: EdgeInsets.only(
+              //           left: AppDimensions.paddingMedium.w,
+              //           right: AppDimensions.paddingExtraLarge.w,
+              //         ),
+              //         child: SvgPicture.asset(AppAssets.buildingApartmentIcon, width: 20.w),
+              //       ),
+              //       enabledBorder: OutlineInputBorder(
+              //         borderRadius: BorderRadius.circular(
+              //           AppDimensions.borderRadiusMedium,
+              //         ),
+              //         borderSide: BorderSide(color: AppColors.white, width: 1.w),
+              //       ),
+              //
+              //       counter: const SizedBox(),
+              //       contentPadding: EdgeInsets.symmetric(
+              //         vertical: 16.h,
+              //         horizontal: AppDimensions.paddingLarge.w,
+              //       ),
+              //     ),
+              //   ),
+              // ),
+              Expanded(child: AnimatedHintField()),
               // SizedBox(width: 4.w),
               // GestureDetector(
               //   onTap: () {
@@ -134,6 +135,114 @@ class _HeaderWidgetState extends State<HeaderWidget> {
           ),
         );
       }
+    );
+  }
+}
+
+class AnimatedHintField extends StatefulWidget {
+  @override
+  State<AnimatedHintField> createState() => _AnimatedHintFieldState();
+}
+
+class _AnimatedHintFieldState extends State<AnimatedHintField>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
+
+  int index = 0;
+
+  final hints = [
+    'find_an_ATM'.tr,
+    'find_an_branch'.tr,
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 20),
+    );
+
+    _animation = Tween<Offset>(
+      begin: Offset(1, 0), // справа
+      end: Offset(0, 0),
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+
+    _startLoop();
+  }
+
+  void _startLoop() async {
+    while (mounted) {
+      await _controller.forward(); // 👉 анимация входа
+
+      await Future.delayed(Duration(seconds: 2)); // пауза
+
+      if (!mounted) return;
+
+      // await _controller.reverse(); // 👉 уехал влево
+
+      if (!mounted) return;
+
+      setState(() {
+        index = (index + 1) % hints.length; // смена текста
+      });
+
+      _controller.reset(); // подготовка к следующему
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Get.toNamed(WebViewScreen.route);
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: AppDimensions.paddingLarge.w,
+          vertical: AppDimensions.paddingLarge.h,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(
+            AppDimensions.borderRadiusMedium,
+          ),
+          border: Border.all(color: AppColors.white, width: 1.w),
+          color: AppColors.inputFillBackground,
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: ClipRect(
+                child: SlideTransition(
+                  position: _animation,
+                  child: Text(
+                    hints[index],
+                    style: TextStyle(
+                      fontSize: 14.sp,
+                      color: AppColors.grey,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SvgPicture.asset(
+              AppAssets.buildingApartmentIcon,
+              width: 20.w,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
